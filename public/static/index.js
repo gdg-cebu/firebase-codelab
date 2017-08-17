a62f82982f649291930498b4d2ce6949360bbade
Emmanuel Lodovice <name3anad@gmail.com>
2017-08-18 00:44:25 +0800

    enable send email verification

--- public/static/index.js
+++ public/static/index.js
@@ -1,10 +1,12 @@
 var index = {
     init: function() {
+        var _this = this;
         firebase.auth().onAuthStateChanged(function(user) {
             if (!user) {
                 window.location = '/login';
             } else {
                 $('.chat').removeClass('hidden');
+                _this.enableVerificationEmail();
             }
         });
         this.enableLogout();
@@ -14,6 +16,22 @@ var index = {
         $('.logout-btn').on('click', function(e) {
             firebase.auth().signOut();
         });
+    },
+
+    enableVerificationEmail: function() {
+        var user = firebase.auth().currentUser;
+        if (user.emailVerified === false) {
+            $('.alert').removeClass('hidden');
+        }
+
+        $('.resend-link').on('click', function(e) {
+            e.preventDefault();
+            user.sendEmailVerification().then(function() {
+                $('.resend-link').text('sent');
+            }).catch(function(error) {
+                console.error(error);
+            });
+        })
     }
 }
 
